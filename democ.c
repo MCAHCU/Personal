d1e66355f9daae5fed9a40ff5348463ac2183f15
AnujRakesh <anujrakesh@anujrakesh-K53U.(none)>
2015-09-04 19:44:00 +0530

    demo

---
+++ democ.c
@@ -0,0 +1,32 @@
+/*#include <stdio.h>
+
+int main()
+{
+	int a, b, c, abc=0;
+	a = b = c = 40;
+
+	if(c) 
+	{
+		int abc;
+		abc = a+b*c;
+	}
+	printf("c=%d, abc=%d",c,abc);
+	return 0;
+}*/
+
+#include <stdio.h>
+
+/*int main()
+{
+	int a=4,b=2;
+	//a=b<<a + b>>2;
+	a = b>>2;
+	printf("%d\n",a);
+	return 0;
+}*/
+
+int main()
+{
+	printf("Hello demo");
+	return 0;
+}
